024677b2ef8be2532ce08d07811b52e27c1836f1
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2021-09-28 21:25:33 -0400

    brigadier sucks

--- src/main/java/us/jcedeno/DedsafioProxy.java
+++ src/main/java/us/jcedeno/DedsafioProxy.java
@@ -1,11 +1,20 @@
 package us.jcedeno;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 
 import org.slf4j.Logger;
 
+import us.jcedeno.commands.BingoCommands;
+
+/**
+ * DedsafioProxy
+ * 
+ * @author jcedeno
+ */
 @Plugin(id = "dedsafio-bingo", name = "Dedsafio Bingo", version = "1.0", authors = { "jcedeno" })
 public class DedsafioProxy {
     private final ProxyServer server;
@@ -15,6 +24,11 @@ public class DedsafioProxy {
     public DedsafioProxy(ProxyServer server, Logger logger) {
         this.server = server;
         this.logger = logger;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent e) {
+        server.getCommandManager().register(BingoCommands.getBrigadierCommand());
     }
 
 }
---
+++ src/main/java/us/jcedeno/commands/BingoCommands.java
@@ -0,0 +1,28 @@
+package us.jcedeno.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.velocitypowered.api.command.BrigadierCommand;
+import com.velocitypowered.api.command.CommandSource;
+
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+public class BingoCommands {
+
+    public static BrigadierCommand getBrigadierCommand() {
+        var dispatched = new CommandDispatcher<CommandSource>();
+
+        LiteralArgumentBuilder<CommandSource> c = dispatched.register(LiteralArgumentBuilder.<CommandSource>literal("te")
+                .then(RequiredArgumentBuilder.argument("bar", StringArgumentType.word()).executes(c -> {
+                    if (c.getSource()instanceof CommandSource source) {
+                        source.sendMessage(MiniMessage.get().parse("<red>" + c.getArgument("bar", String.class)));
+                    }
+                    return 1;
+                })));
+
+        return new BrigadierCommand(c);
+    }
+
+}
